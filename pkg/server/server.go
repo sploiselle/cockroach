c00306f8ca045c8b7514099b9c58b8a79031e74a
Sean Loiselle <himself@sean.com>
2018-09-01 18:57:34 -0400

    sinlge node gossip

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1502,8 +1502,10 @@ func (s *Server) Start(ctx context.Context) error {
 	s.tsDB.PollSource(
 		s.cfg.AmbientCtx, s.recorder, DefaultMetricsSampleInterval, ts.Resolution10s, s.stopper,
 	)
+
+	// If flag set, begin monitoring metrics.
 	if s.cfg.MonitorMetrics != "" {
-		s.tsMonitor = ts.NewMonitor(s.tsDB, s.recorder.GetMetricsMetadata(), s.st, s.cfg.MonitorMetrics)
+		s.tsMonitor = ts.NewMonitor(s.NodeID(), s.tsDB, s.gossip, s.recorder.GetMetricsMetadata(), s.st, s.cfg.MonitorMetrics)
 		s.tsMonitor.Start()
 	} else {
 		fmt.Println("no monitor, bucko")
